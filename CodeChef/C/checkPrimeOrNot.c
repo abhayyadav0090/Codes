986e1b90c7cba9217ea6c378645eee686c6defd7
Abhay Yadav <abhayyadav19955@gmail.com>
2026-07-06 12:51:33 +0530

    06/07/2026

---
+++ CodeChef/C/checkPrimeOrNot.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int num, count=0;
+    scanf("%d", &num);
+    for(int i=1; i<=num; i++){
+        if(num%i==0){
+            count++;
+        }
+    }
+    if(count==2) printf("YES");
+    else printf("NO");
+    return 0;
+}
