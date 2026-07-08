7e2600c174ca2e70f301126aa395ea0899751ec6
Abhay Yadav <abhayyadav19955@gmail.com>
2026-07-08 07:50:37 +0530

    06/07/2026

---
+++ Strivers/LearnC++4BacicsOfArrayStrings.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    // Array is a collection of similar data types. It is a data structure that can hold a fixed number of values of a single type. The values are stored in contiguous memory locations, and each value can be accessed using an index.
+    // char arr[5];
+    // long long arr[5];
+    int arr[5]={1,2,3,4,5};
+    // cin >> arr[0] >> arr[1] >> arr[2] >> arr[3] >> arr[4];
+    cout << arr[0] << " " << arr[1] << " " << arr[2] << " " << arr[3] << " " << arr[4] << endl;
+    arr[3] = 100;
+    cout << arr[3] << endl;
+    // arr[3]=7.7 -> output: 7 (decimal part is truncated)
+
+    // 2D Array
+    int array[3][5]; // 3 Rows and 5 Columns
+    array[1][3]=78;
+    cout << array[1][2] << endl; // Garbage value
+
+    string s = "Abhay";
+    cout << s[0] << endl;
+    cout << s+" "+"Yadav" << endl;
+    return 0;
+}
