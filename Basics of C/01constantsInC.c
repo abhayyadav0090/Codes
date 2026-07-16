b705d26edf2c1e72175f2b54d4d083a8e2fe0bbb
Abhay Yadav <abhayyadav19955@gmail.com>
2026-07-16 22:43:23 +0530

    16/07/2026

---
+++ Basics of C/01constantsInC.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+/*
+    constants{
+        Numeric Constants{
+            Integers{
+                decimal {10, +10, -10, 0, 1234567890}
+                octal {012, 077, 0123}
+                hexadecimal {0x1A, 0XFF, 0x123abc}
+            }
+            Floating / Real constants{
+                12.56, +12.56, -12.56, 12.56e2, 12.56E-2, (not valid -> 12.56e+2, 12.56E+2, $12.56, 12.56e2.5)
+            }
+        }
+        Character Constants{
+            Single Character Constants {
+                'A', 'b', 'C'
+                stored as integer values in memory -> ASCII values -> 'A' = 65, 'B' = 66, 'C' = 67, 'a' = 97, 'b' = 98, 'C' = 67
+                A-Z -> 65-90
+                a-z -> 97-122
+                0-9 -> 48-57
+                special characters -> 32-47, 58-64, 91-96, 123-126
+                5 != '5' -> 5 is an integer constant, '5' is a character constant
+            }
+            String Constants {"Hello", "World"}
+        }
+    }
+*/
+
+int main() {
+    printf("Hello World!\n");
+    return 0;
+}
