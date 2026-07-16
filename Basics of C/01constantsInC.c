87828df9eb4cb670ed51634839097a2438191cbc
Abhay Yadav <abhayyadav19955@gmail.com>
2026-07-16 22:51:24 +0530

    Revising C Language

--- Basics of C/01constantsInC.c
+++ Basics of C/01constantsInC.c
@@ -22,12 +22,28 @@
                 special characters -> 32-47, 58-64, 91-96, 123-126
                 5 != '5' -> 5 is an integer constant, '5' is a character constant
             }
-            String Constants {"Hello", "World"}
+            String Constants {
+                "Hello", "World", "ab$", "a", " "
+                1 != '1' != "1" -> 1 is an integer constant, '1' is a character constant, "1" is a string constant
+                string length = number of characters in string + 1 (for null character)
+                stored as array of characters in memory -> "Hello" = {'H', 'e', 'l', 'l', 'o', '\0'}
+            }
         }
     }
 */
 
-int main() {
+
+#define PI 3.14 // preprocessor constant
+#define MAX 100 // preprocessor constant
+#define A 10 // preprocessor constant
+
+void main() {
     printf("Hello World!\n");
-    return 0;
+    const int a = 10; // integer constant
+    // a =20; // error: assignment of read-only variable 'a'
+    printf("Value of a: %d\n", a);
+    printf("Value of PI: %f\n", PI);
+    printf("Value of MAX: %d\n", MAX);
+    printf("Value of A: %d\n", A);
+    // return 0;
 }
